43516ad68d1a69d430a5f072ee84d40bf982ae68
stxss <93729036+stxss@users.noreply.github.com>
2023-02-06 20:23:10 +0000

    Modify main file to just initiate the game

--- lib/main.rb
+++ lib/main.rb
@@ -1,20 +1,5 @@
-class Hangman
-
-    def initialize
-        @secret_word = get_word
-        puts "the selected was '#{@secret_word}' and its length is #{@secret_word.length}"
-    end
-
-    def get_word
-        word = ""
-        until (5..12).cover?(word.length)
-           word = open("google-10000-english.txt").readlines.sample.strip
-        end
-        word
-    end
-end
-
-
-
+require_relative "display"
+require_relative "intro"
+require_relative "hangman"
 
 Hangman.new
