7fe6b98560ea751fcffc197b2ee6e228b497970f
stxss <93729036+stxss@users.noreply.github.com>
2023-02-07 20:10:42 +0000

    Mofidy to only contain methods pertaining to the display

--- lib/display.rb
+++ lib/display.rb
@@ -1,21 +1,11 @@
-class Display
-  attr_reader :word, :length
-  attr_accessor :guesses, :word_encrpyted
-
-  def initialize(word)
-    @word = word
-    @length = word.length
-    @guesses = 6
-    @word_encrpyted = "_" * @length
-    print_board
-  end
-
-  def print_board
+module Display
+  def print_board(to_print)
     system("clear")
-    puts "                      |            "
-    puts "                      | #{guesses} Guesses Left  "
-    puts "   #{word_encrpyted}                   +____________________"
+    puts "     +----------------+                          "
+    puts "     | #{@guesses} Guesses Left | "
+    puts "     +----------------+"
     puts "                                   "
+    puts "          #{to_print}           "
     puts "                                   "
     puts "                                   "
     puts "                                   "
