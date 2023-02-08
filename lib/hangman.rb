6a5c32947fc29c2e6b3e4e3042f773f85954e26f
stxss <93729036+stxss@users.noreply.github.com>
2023-02-08 12:25:31 +0000

    Add self-documentation and refactor a bit

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,7 +2,9 @@ require "io/console"
 require_relative "logic"
 require_relative "display"
 
+# Class to initiate the game
 class Hangman
+  # Include the modules for the game logic and the display
   include GameLogic
   include Display
 
@@ -19,39 +21,39 @@ class Hangman
     @incorrect_letters = []
     @type_of_game = ""
 
+    # Print the introduction and prompt to choose if the player wants to start a new game or load an already saved game
     Intro.new
     choose_type
 
-    # New game
+    # If the type is 1, create a new game
     if @type_of_game == "1"
+      # Set up the first board
       print_board(word_encrpyted)
+
+      # Ask for guesses until the user wins or is out of guesses
       loop do
         ask_guess
         break if @is_winner || @game_end
       end
     elsif @type_of_game == "2"
-      # Load game
+      # If it's 2, load a saved game
       puts "loading!"
     end
 
+    # If the user wins, print a congratulatory message
     if @is_winner
       puts "Congrats! You guessed the code!"
     elsif @game_end && !@is_winner
+      # If the user hasn't won yet and is out of guesses, it means they lost, so print out an encouraging message
       puts "You lost! The word was '#{@secret_word}'"
       puts "Better luck next time!"
     end
+    # Reset the current instance of the game and prompt for a restart of the game
     delete
     restart
   end
 
-  def get_word
-    word = ""
-    until (5..12).cover?(word.length)
-      word = open("google-10000-english.txt").readlines.sample.strip
-    end
-    word
-  end
-
+  # Method to choose the type of game
   def choose_type
     loop do
       @type_of_game = gets.chomp
@@ -60,6 +62,17 @@ class Hangman
     end
   end
 
+  # Method to get the random secret word from the list
+  def get_word
+    word = ""
+    # The word has to have at least 5 letters and no more than 12
+    until (5..12).cover?(word.length)
+      word = open("google-10000-english.txt").readlines.sample.strip
+    end
+    word
+  end
+
+  # Method to delete/reset the current variables, effectively resetting the class
   def delete
     @secret_word = nil
     @is_winner = nil
