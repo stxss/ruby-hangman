d9937fc3b7118a04f638ab38fcb943f18b512509
stxss <93729036+stxss@users.noreply.github.com>
2023-02-08 12:17:48 +0000

    Do some style alterations

--- lib/intro.rb
+++ lib/intro.rb
@@ -1,5 +1,6 @@
 require_relative "text_styles"
 
+# Class for the introduction
 class Intro
   using TextStyles
   def initialize
@@ -10,11 +11,11 @@ class Intro
     puts <<~HEREDOC
       #{"Welcome to Hangman!".bold.italic.underlined}
 
-      This is a game where you have to guess your opponent's secret word within a limited number of turns.
+      This is a game where you have to guess your opponent's #{"secret word".underlined} within a #{"limited number of turns".underlined}.
 
-      If the guessing player suggests a letter which occurs in the word, that letter is written in all its correct positions.
+      If the guessing player suggests a letter which #{"occurs".bold.italic.underlined} in the word, that letter is written in all its correct positions.
 
-      If the suggested letter does not occur in the word, a tally mark is added until it completes a stick figure.
+      If the suggested letter #{"does not occur".bold.italic.underlined} in the word, your guesses are decremented by 1 point.
 
       Generally, the game ends once the word is guessed, or if all guesses have been used.
 
@@ -22,11 +23,11 @@ class Intro
 
       If the word is correct, the game is over and the guesser wins.
 
-      Select the option that you would like to play
+      Select the option that you would like to play:
 
-      [1] Play a new game
-      [2] Load an existing game
-      
+      #{"[1]".bold.fg_color(:pink)} Play a new game
+      #{"[2]".bold.fg_color(:pink)} Load an existing game
+
     HEREDOC
   end
 end
